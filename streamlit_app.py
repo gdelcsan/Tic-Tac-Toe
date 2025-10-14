fb300acd11076be6196cc39d7a8d55b446fa4f15
Gaby <gabrieladelcristo@gmail.com>
2025-10-14 10:33:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -322,7 +322,7 @@ info = st.container()
 with info:
     left, right = st.columns([1,1])
     with left:
-        st.subheader("Game Info")
+        st.markdown('<p style="color: #FFFFFF; font-size: 24px;">Game Info</p>', unsafe_allow_html=True)
         if st.session_state.winner == "Draw":
             st.success("It's a draw!")
         elif st.session_state.winner in ["X", "O"]:
